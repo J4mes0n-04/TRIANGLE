4282e383b48d09cad286116bb8712bdf63c7b733
Maxim <maksgmq@mail.ru>
2021-10-03 09:56:10 +0500

    Done

---
+++ src/main/java/geekbrains/homework4/TriangleType.java
@@ -0,0 +1,5 @@
+package geekbrains.homework4;
+
+public enum TriangleType {
+    INVALID, SCALENE, EQUILATERAL, ISOSCELES
+}
---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntegrationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Integraion test1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        System.out.println("Integraion test2");
+    }
+}
